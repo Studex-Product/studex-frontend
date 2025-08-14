10987e5fe6153b5dd75c126bc028e8cdfdb9cd59
Anita David <anitafunmilayo@gmail.com>
2025-08-14 14:21:17 +0100

    feat:Implement Admin Login Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./pages/Home";
+import AdminLogin from "./components/auth/AdminLogin";
 
 const queryClient = new QueryClient();
 
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/admin/login" element={<AdminLogin/>}/>
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
