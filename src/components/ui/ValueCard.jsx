c5e7431e097311f21f77a016af49c5b3889f7e31
Anita David <anitafunmilayo@gmail.com>
2025-08-23 16:13:58 +0100

    Refactor: applied requested adjustments

--- src/components/ui/ValueCard.jsx
+++ src/components/ui/ValueCard.jsx
@@ -1,11 +1,12 @@
 function ValueCard({ icon, title, description }) {
   return (
-    <div className="flex flex-col justify-center items-center text-center p-6 hover:shadow-md transition-shadow duration-300 ease-in-out transform hover:-translate-y-1">
+    <div className="flex flex-col justify-center items-center text-center p-6 ">
+      
       <img src={icon} alt={title} className="w-12 h-12 mb-4" />
 
-      <p className="font-semibold text-lg text-gray-900">{title}</p>
+      <p className="font-semibold lg:text-[22px] text-[20px] text-gray-700">{title}</p>
 
-      <p className="text-sm text-gray-600 mt-2 leading-relaxed max-w-xs">
+      <p className="text-[14px] lg:text-[16px] text-gray-600 mt-2 leading-relaxed max-w-xs">
         {description}
       </p>
     </div>
